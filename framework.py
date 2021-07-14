3ac1340deeb61b9f6fafed2e1f421a241e3e3a77
Harrison McCarty <mccarth@purdue>
2021-07-13 17:29:07 -0700

    added helper

--- framework.py
+++ framework.py
@@ -1,7 +1,26 @@
 from sim import Sim
 from cv2 import ORB_create, drawKeypoints
+import numpy as np
 
+# Global defs
 sim = Sim(headless=True)
+
+
+def calc_distances(p0, points):
+    return ((p0 - points)**2).sum(axis=1)
+
+
+def min_subset(pts, K):
+    farthest_pts = np.zeros((K, 3))
+    farthest_idxs = []
+    farthest_pts[0] = pts[np.random.randint(len(pts))]
+    distances = calc_distances(farthest_pts[0], pts)
+    for i in range(1, K):
+        farthest_idxs.append(np.argmax(distances))
+        farthest_pts[i] = pts[farthest_idxs[-1]]
+        distances = np.minimum(distances, calc_distances(farthest_pts[i], pts))
+    return farthest_idxs
+
 
 # Receive frames
 rgb, depth = sim.step()
